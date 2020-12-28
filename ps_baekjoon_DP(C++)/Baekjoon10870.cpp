17acfb9f77b6dc6a320656e90421a010821dbea9
J-H99 <esinfam99@g.skku.edu>
2020-12-28 16:42:30 +0900

    Baekjoon_DP(C++)

---
+++ ps_baekjoon_DP(C++)/Baekjoon10870.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+using LL = long long;
+
+int main() {
+
+    int n;
+    cin >> n;
+    LL Fn, Fn_1 = 1, Fn_2 = 0;
+    
+    if (n == 0) {
+        cout << 0;
+    }
+    
+    else if (n == 1) {
+        cout << 1;
+    }
+
+    else {
+        for (int i = 2; i <= n; i++) {
+            Fn = Fn_1 + Fn_2;
+            Fn_2 = Fn_1;
+            Fn_1 = Fn;
+        }    
+        cout << Fn;
+    }
+    return 0;
+}
