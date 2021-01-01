43bd140f42d3445ec8381623c3cf2941d566b0f0
J-H99 <esinfam99@g.skku.edu>
2021-01-01 16:32:59 +0900

    Baekjoon_DP(20200101)

---
+++ ps_baekjoon_DP(C++)/Baekjoon1904.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int* DP = new int[N];
+    if (N == 1) {
+        cout << 1;
+    }
+
+    else if (N == 2) {
+        cout << 2;
+    }
+
+    else {
+        DP[0] = 1;
+        DP[1] = 2;
+
+        for (int i = 2; i < N; i++) {
+            DP[i] = DP[i-1] + DP[i-2];
+            DP[i] %= 15746;
+        }
+
+        cout << DP[N-1];
+    }
+    return 0;
+}
