b582fe11b5b90473a5c3dca2b5da08b3fd326651
J-H99 <esinfam99@g.skku.edu>
2021-01-04 13:13:46 +0900

    Baekjoon_DP(20210104)

---
+++ ps_baekjoon_DP(C++)/Baekjoon9251(Not 2nd Array).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int before = 0;
+    string first, second;
+    cin >> first >> second;
+    
+    first = "0" + first;
+    second = "0" + second;
+
+    int* sequence = new int[second.length()];
+    memset(sequence, 0, sizeof(int) * second.length());
+
+    for(int i = 1; i < first.length(); i++) {
+        before = 0;
+        for(int j = 1; j < second.length(); j++) {
+            if (sequence[j] > before) { 
+                before = sequence[j];
+            }
+            
+            else if (first[i] == second[j]) {
+                sequence[j] = before + 1;
+            }
+        }
+    }
+
+    cout << *max_element(sequence, sequence + second.length());
+    return 0;
+}
---
+++ ps_baekjoon_DP(C++)/Baekjoon9251.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int max_value = 0, max_cand;
+    string first, second;
+    cin >> first >> second;
+
+    first = "0" + first;
+    second = "0" + second;
+
+    int** dp_sequence = new int*[second.length()];
+    for(int i = 0; i < second.length(); i++) {
+        dp_sequence[i] = new int[first.length()];
+        memset(dp_sequence[i], 0, sizeof(int)* first.length());
+    }
+    
+    for (int i = 1; i < second.length(); i++) {
+        for (int j = 1; j < first.length(); j++) {
+            if (second[i] == first[j]) {
+                dp_sequence[i][j] = dp_sequence[i-1][j-1] + 1;
+            }
+
+            else {
+                dp_sequence[i][j] = max(dp_sequence[i-1][j], dp_sequence[i][j-1]);
+            }
+        }
+    }
+
+    cout << *max_element(dp_sequence[second.length() - 1], dp_sequence[second.length() - 1] + first.length());
+    return 0;
+}
