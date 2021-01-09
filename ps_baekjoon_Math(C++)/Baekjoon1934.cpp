7c787e4ff3a296252e85b6e02dd13919e65e71f7
J-H99 <esinfam99@g.skku.edu>
2021-01-09 22:00:09 +0900

    Baekjoon_Math(20210109)

---
+++ ps_baekjoon_Math(C++)/Baekjoon1934.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+using LL = long long;
+
+LL gcd(int A, int B) {
+    if (B == 0) {
+        return A;
+    }
+
+    if (A < B) {
+        int temp = A;
+        A = B;
+        B = A;
+    }
+
+    return gcd(B, A % B);
+}
+
+int main() {
+    int T, A, B;
+    cin >> T;
+
+    LL* result = new LL[T];
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+        result[i] = A * B / gcd(A, B);
+    }
+
+    for(int i = 0; i < T; i++) {
+        cout << result[i] << endl;
+    }
+    delete result;
+    return 0;
+}
