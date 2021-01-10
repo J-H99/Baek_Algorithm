69b531a6046e5358e2b82dc3fc257df3a52379d7
J-H99 <esinfam99@g.skku.edu>
2021-01-10 17:21:21 +0900

    Baekjoon_Math(20210110)

---
+++ ps_baekjoon_Math(C++)/Baekjoon1037.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int x, int y) {
+    return x < y;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    int* trivial_divisor = new int[N];
+    for (int i = 0; i < N; i++) {
+        cin >> trivial_divisor[i];
+    }
+
+    sort(trivial_divisor, trivial_divisor + N, compare);
+
+    cout << trivial_divisor[0] * trivial_divisor[N - 1];
+}
